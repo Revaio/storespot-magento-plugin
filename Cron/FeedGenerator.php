721f029a293f1a971ac79c12eb46324daca8c515
axelvdw <axel.v.d.wallen@gmail.com>
2018-12-20 11:13:36 +0100

    Feed generation in cron

--- Cron/FeedGenerator.php
+++ Cron/FeedGenerator.php
@@ -1,43 +1,20 @@
 <?php
 namespace StoreSpot\Personalization\Cron;
 
-use \Psr\Log\LoggerInterface;
-
 class FeedGenerator {
-    protected $_logger;
-//    protected $_feed;
-
-//    protected $_directoryList;
-//    protected $_file;
-
+    protected $feed;
+    
     public function __construct(
-        LoggerInterface $logger
-//        \Magento\Framework\App\Filesystem\DirectoryList $directoryList,
-//        \Magento\Framework\Filesystem\Io\File $file
+        \StoreSpot\Personalization\Model\Feed $feed
     )
     {
-        $this->_logger = $logger;
-//        $this->_directoryList = $directoryList;
-//        $this->_file = $file;
+        $this->feed = $feed;
     }
 
     public function execute() {
-        $this->_logger->debug('New feed generation');
         error_log('New feed generation');
-//        $url = "https://webkul.com/wp-content/themes/webkul/inc/E-Commerce-Marketplace-Use-Case.pdf";
-//        $pdfContent = file_get_contents($url);
-//
-//        $filePath = "/storespot/";
-//        $pdfPath = $this->_directoryList->getPath('media').$filePath;
-//        if (!is_dir($pdfPath))
-//        {
-//            $ioAdapter = $this->_file;
-//            $ioAdapter->mkdir($pdfPath, 0775);
-//        }
-//
-//        $fileName = "webkul.pdf";
-//        $ioAdapter->open(array('path'=>$pdfPath));
-//        $ioAdapter->write($fileName, $pdfContent, 0666);
+        $this->feed->createFeed();
+        error_log('Done with the feed');
     }
 
 }
