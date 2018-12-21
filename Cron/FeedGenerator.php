a0740b4bdf58aa587d77a24e2b993a00d6cb3261
Jeroen Faes <faesjeroen@gmail.com>
2018-12-21 11:06:28 +0100

    Remove legacy

--- Cron/FeedGenerator.php
+++ Cron/FeedGenerator.php
@@ -3,26 +3,16 @@ namespace StoreSpot\Personalization\Cron;
 
 class FeedGenerator {
     protected $feed;
-    protected $helperData;
-    protected $logger;
 
     public function __construct(
-        \StoreSpot\Personalization\Model\Feed $feed,
-        \StoreSpot\Personalization\Helper\Data $helperData,
-        \Psr\Log\LoggerInterface $logger
+        \StoreSpot\Personalization\Model\Feed $feed
     )
     {
         $this->feed = $feed;
-        $this->helperData = $helperData;
-        $this->logger = $logger;
     }
 
     public function execute() {
         $this->feed->createFeed();
-        $last = date('Y-m-d H:i:s');
-        $next = date('Y-m-d H:i:s', (time() + 60 * 60));
-        $this->helperData->saveCronConfig('last_execution', $last);
-        $this->helperData->saveCronConfig('next_execution', $next);
     }
 
 }
