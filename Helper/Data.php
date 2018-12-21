9cda971233c5b38ee29f89af10bf867b3ea8555f
Jeroen Faes <faesjeroen@gmail.com>
2018-12-21 10:44:34 +0100

    Remove cron stuff

--- Helper/Data.php
+++ Helper/Data.php
@@ -4,7 +4,6 @@ namespace StoreSpot\Personalization\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Store\Model\ScopeInterface;
-use Magento\Framework\App\Config\Storage\WriterInterface;
 
 
 class Data extends AbstractHelper
@@ -17,20 +16,9 @@ class Data extends AbstractHelper
         return $this->scopeConfig->getValue($field, ScopeInterface::SCOPE_STORE, $storeId);
     }
 
+    // General settings
     public function getGeneralConfig($code, $storeId = null)
     {
         return $this->getConfigValue(self::XML_PATH_STORESPOT . 'general/' .$code, $storeId);
-    }
-
-    public function getCronConfig($code, $storeId = null)
-    {
-        return $this->getConfigValue(self::XML_PATH_STORESPOT . 'cron/' .$code, $storeId);
-    }
-
-    public function saveCronConfig($key, $value, WriterInterface $configWriter)
-    {
-        error_log($key);
-        $configWriter->save(self::XML_PATH_STORESPOT . 'cron/' . $key, $value);
-        error_log('ok?');
     }
 }
